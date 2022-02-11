88abdd650f7d4f94af826ca1bc49efa070384a2e
kaitlynsteeves <kaitlynsteeves@hotmail.com>
2022-02-11 15:13:34 -0500

    Added login page and routes

--- cinemaswipe/src/App.js
+++ cinemaswipe/src/App.js
@@ -1,12 +1,27 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, HashRouter, Route, Routes } from 'react-router-dom';
+
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
 
 function App() {
+  const [token, setToken] = useState();
+
+  /* If the user has not signed in yet, show login page*/
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+    <div className="wrapper">
+        <Router>
+          <Routes>
+            <Route exact path="/" component={Home} />
+
+          </Routes>
+        </Router>
     </div>
   );
 }
