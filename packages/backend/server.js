d2079449a1624e1ca3ea271b07e2fa03e01f4264
jsubang <em.subang@gmail.com>
2022-02-12 16:56:25 -0500

    milestone1

--- packages/backend/server.js
+++ packages/backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const path = require("path");
 
 //3000 port for local, heroku's port depeneds on the server environment variable
 const port = process.env.PORT || 8080;
@@ -8,14 +9,18 @@ const port = process.env.PORT || 8080;
 //     console.log(`Server running on port${port}`);
 // });
 
+const buildPath = "../react/build";
+
+app.use(express.static(path.join(__dirname, buildPath)));
+
 app.set("port", port);
 
 app.use(cors());
 
 app.get("/", (req, res) => {
-    res.send("hi");
+    // res.send("hi");
+    res.sendFile(path.join(__dirname, buildPath, "index.html"));
 });
-
 
 app.use("/login", (req, res) => {
     /* TODO: authenticate in database, create unique token */
@@ -33,12 +38,8 @@ app.use("/createUser", (req, res) => {
     });
 });
 
-
 app.get("*", (req, res) => {
     res.send("404, RIP");
 });
 
-
-app.listen(port, () =>
-    console.log(`API is running on port${port}`)
-);
+app.listen(port, () => console.log(`API is running on port${port}`));
