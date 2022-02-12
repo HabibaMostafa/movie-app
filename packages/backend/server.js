c9ae1b91ec0b54dbda8b34a861076a40fc60f27d
jsubang <em.subang@gmail.com>
2022-02-12 14:25:34 -0500

    ready for heroku deploy

--- packages/backend/server.js
+++ packages/backend/server.js
@@ -1,23 +1,31 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
+
+//3000 port for local, heroku's port depeneds on the server environment variable
+const port = process.env.PORT || 8080;
+// app.listen(port, () => {
+//     console.log(`Server running on port${port}`);
+// });
 
 app.use(cors());
 
-app.use('/login', (req, res) => {
-  /* TODO: authenticate in database, create unique token */
+app.use("/login", (req, res) => {
+    /* TODO: authenticate in database, create unique token */
 
     res.send({
-      token: 'test123'
+        token: "test123",
     });
 });
 
-app.use('/createUser', (req, res) => {
-  /* TODO: create in database, create unique token */
+app.use("/createUser", (req, res) => {
+    /* TODO: create in database, create unique token */
 
     res.send({
-      token: 'test123'
+        token: "test123",
     });
 });
-  
-app.listen(8080, () => console.log('API is running on http://localhost:8080/login'));
+
+app.listen(port, () =>
+    console.log(`API is running on http://localhost:${port}/login`)
+);
