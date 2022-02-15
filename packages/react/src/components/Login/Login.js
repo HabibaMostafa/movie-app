f93e676fe8fa4e8fe546f9efd00fcaaefd293143
Habiba Mostafa <mostafah@uoguelph.ca>
2022-02-15 12:57:58 -0500

    Changed login input font color to black

--- packages/react/src/components/Login/Login.js
+++ packages/react/src/components/Login/Login.js
@@ -37,14 +37,14 @@ export default function Login({ setToken }) {
             <form onSubmit={handleSubmit}>
                 <label>
                     <p>Username</p>
-                    <input
+                    <input className="login-input"
                         type="text"
                         onChange={(e) => setUserName(e.target.value)}
                     />
                 </label>
                 <label>
                     <p>Password</p>
-                    <input
+                    <input className="login-input"
                         type="password"
                         onChange={(e) => setPassword(e.target.value)}
                     />
