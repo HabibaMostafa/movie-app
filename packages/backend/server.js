af0809bd6763b89ef09a4c6afbba646ea70e817a
jsubang <em.subang@gmail.com>
2022-02-15 21:53:03 -0500

    fixed imports/ local install modules

--- packages/backend/server.js
+++ packages/backend/server.js
@@ -1,14 +1,20 @@
-import MongoDb from './utils/database';
-import sha256 from 'sha256'
-import crypto from 'crypto'
-import bcrypt from 'bcrypt'
+// import MongoDb from './utils/database';
+// import sha256 from 'sha256'
+// import crypto from 'crypto'
+// import bcrypt from 'bcrypt'
+
+const MongoDb = require("./utils/database");
+const { tmdb } = require("./utils/tmdb");
+
+// const sha256 = require("sha256");
+
+const crypto = require("crypto");
+const bcrypt = require("bcrypt");
 
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const path = require("path");
-
-const {tmdb} = require("./utils/tmdb");
 
 //3000 port for local, heroku's port depeneds on the server environment variable
 const port = process.env.PORT || 8080;
@@ -30,68 +36,74 @@ app.get("/", (req, res) => {
 });
 
 // -- Login --
-app.use("/login", (req, res) => {
+app.use("/login", async (req, res) => {
     /* TODO: authenticate in database, create unique token */
 
     // mongodb
     const mongoDb = new MongoDb();
     mongoDb.start(() => {
-        console.log("mongoDb ready")
+        console.log("mongoDb ready");
     });
 
     try {
         let password = req.body.password;
         let username = req.body.username;
 
-        let find_param = {'name':username}
+        let find_param = { name: username };
 
         //find user
         let results = null;
-        results = await mongoDb.collection("users").findOne(find_param)
+        // results = mongoDb.collection("users").findOne(find_param);
+        results = await mongoDb.collection("users").findOne(find_param);
 
         if (!results) {
-            throw Error("no such user found")
+            throw Error("no such user found");
         }
 
         //check password
-        if (!results.password){
-            throw Error("something must be wrong")
+        if (!results.password) {
+            throw Error("something must be wrong");
         }
 
         // let password2 = sha256(password)
-        let password2 = crypto.createHash('sha256').update(password).digest('hex');
-        let saved_hash = results.password
+        let password2 = crypto
+            .createHash("sha256")
+            .update(password)
+            .digest("hex");
+        let saved_hash = results.password;
 
-        results = null
-        results = await bcrypt.compare(password2, saved_hash)
+        results = null;
+        // results = bcrypt.compare(password2, saved_hash);
+        results = await bcrypt.compare(password2, saved_hash);
         if (results !== true) {
-            throw Error("password is not okay")
+            throw Error("password is not okay");
         }
 
         //create login token
-        let login_token = makeid('4') + parseInt(new Date().getTime()).toString(36);
+        let login_token =
+            makeid("4") + parseInt(new Date().getTime()).toString(36);
 
         let upd_param = {
-            '$push':{
-                'loginTokens':login_token
-            }
-        }
+            $push: {
+                loginTokens: login_token,
+            },
+        };
 
         // update mongo
-        await mongoDb.collection("users").update(find_param, upd_param)
+        // mongoDb.collection("users").update(find_param, upd_param);
+        await mongoDb.collection("users").update(find_param, upd_param);
 
         res.send({
             token: login_token,
             //token: "test123",
         });
-
     } catch (err) {
-        throw Error(err.message)
+        throw Error(err.message);
     }
 
     // end mongodb
     mongoDb.end(() => {
-        console.log("mongoDb closed")
+        console.log("mongoDb closed");
     });
 });
 
@@ -103,77 +115,78 @@ app.use("/signUp", (req, res) => {
     // Start mongodb
     const mongoDb = new mongoDb();
     mongoDb.start(() => {
-        console.log("mongoDb ready")
+        console.log("mongoDb ready");
     });
 
     try {
         // 1. Receive username and password
-        let password =  req.body.password;
-        let username =  req.body.username;
+        let password = req.body.password;
+        let username = req.body.username;
 
-        let find_param = {'name':username}
+        let find_param = { name: username };
 
-        let results = null
-        results = await mongoDb.collection("users").count(find_param)
-        if (results != 0){
-            throw Error("user already exist")
+        let results = null;
+        // results = mongoDb.collection("users").count(find_param);
+        results = await mongoDb.collection("users").count(find_param);
+        if (results != 0) {
+            throw Error("user already exist");
         }
 
         // bcrypt of password
         // let password2 = sha256(password)
-        let password2 = crypto.createHash('sha256').update(password).digest('hex');
+        let password2 = crypto
+            .createHash("sha256")
+            .update(password)
+            .digest("hex");
         let bcrypt_hash = bcrypt.hashSync(password2, 10);
 
         // login token which to use login
-        let login_token = makeid('4') + parseInt(new Date().getTime()).toString(36);
+        let login_token =
+            makeid("4") + parseInt(new Date().getTime()).toString(36);
 
         let insert_params = {
             createdAt: new Date(),
             password: bcrypt_hash,
             name: username,
             loginTokens: login_token,
-        }
+        };
 
         // insert into database
-        results = null
-        results = await mongoDb.collection("users").insert(insert_params)
+        results = null;
+        // results = mongoDb.collection("users").insert(insert_params);
+        results = await mongoDb.collection("users").insert(insert_params);
 
-        console.log("results: ", results)
+        console.log("results: ", results);
 
-        if ( results === null ) {
-            throw Error("could not create a user...")
+        if (results === null) {
+            throw Error("could not create a user...");
         }
 
         res.send({
             token: login_token,
         });
-
     } catch (err) {
-        throw Error(err.message)
+        throw Error(err.message);
     }
 
     // end mongodb
     mongoDb.end(() => {
-        console.log("mongoDb closed")
+        console.log("mongoDb closed");
     });
 });
 
 // sample api get request
 app.use("/movies", (req, res) => {
-
-
     // make request to api
-    const test = tmdb({},(error, response)=>{
-
-        if(error) {
-            console.log("Error! =(")
+    const test = tmdb({}, (error, response) => {
+        if (error) {
+            console.log("Error! =(");
             return;
         }
 
         res.send(response);
     });
 });
-
 
 app.get("*", (req, res) => {
     res.send("404, RIP");
--- packages/backend/utils/database.js
+++ packages/backend/utils/database.js
@@ -24,8 +24,6 @@
 //     }
 // );
 
-
-
 /////extra stuff you can ignore//////////
 // example generate a new objectid for a document
 // const id = new ObjectId();
@@ -57,7 +55,6 @@
 //     });
 // }
 
-
 // function test() {
 //     client.connect((error) => {
 //         if (error) {
@@ -75,8 +72,6 @@
 //             });
 //     });
 // }
-
-
 
 //////////////////////////////////////////////////////////////////// CREATE EXAMPLES////////////////////////////////////////////////////////////
 
@@ -141,7 +136,6 @@
 ////////////////////////////////////////////////////////// READ EXAMPLES //////////////////////////////////////////////////////////
 
 // read one document, search by username, if record is not found it is not an error, it will just return null instead of the target document
-
 
 // find by object id, (you need to create a new objectid obj with the target hex id as an argument)
 // "620a87f2bbc55385f89b8b7c" belongs to the puppetmaster document
@@ -214,12 +208,12 @@ const client = new MongoClient(connectionUri, {
 //    );
 //});
 
-
 //import mongodb  from 'mongodb';
 //import { v1 as uuidv1 } from 'uuid';
 
-export default class MongoDb {
-
+// class MongoDb {
+// export default class MongoDb {
+module.exports = class MongoDb {
     constructor() {
         this.url = connectionUri;
         this.db_name = databaseName;
@@ -255,10 +249,8 @@ export default class MongoDb {
     }
 
     collection(collectionName) {
-
-        let mongoDbCollection = this.db.collection(collectionName)
+        let mongoDbCollection = this.db.collection(collectionName);
         let collection = {
-
             //insert : (model) => { //TODO: insert many
             //  return new Promise((resolve, reject) => {
             //    if (!model._id){
@@ -271,69 +263,104 @@ export default class MongoDb {
             //  });
             //},
 
-            update: (find_param, upd_param, option_param) => { //TODO: update many
+            update: (find_param, upd_param, option_param) => {
+                //TODO: update many
                 return new Promise((resolve, reject) => {
-
                     if (!option_param) {
-                        mongoDbCollection.updateOne(find_param, upd_param, (err, result) => {
-                            if (err) { reject(err); }
-                            resolve(result);
-                        });
+                        mongoDbCollection.updateOne(
+                            find_param,
+                            upd_param,
+                            (err, result) => {
+                                if (err) {
+                                    reject(err);
+                                }
+                                resolve(result);
+                            }
+                        );
                     } else {
-                        mongoDbCollection.updateOne(find_param, upd_param, option_param, (err, result) => {
-                            if (err) { reject(err); }
-                            resolve(result);
-                        });
+                        mongoDbCollection.updateOne(
+                            find_param,
+                            upd_param,
+                            option_param,
+                            (err, result) => {
+                                if (err) {
+                                    reject(err);
+                                }
+                                resolve(result);
+                            }
+                        );
                     }
-
                 });
             },
 
-            find: (find_param, sort_param, skip, limit) => { //TODO: search
+            find: (find_param, sort_param, skip, limit) => {
+                //TODO: search
                 return new Promise((resolve, reject) => {
-
                     if (sort_param && skip && limit) {
+                        mongoDbCollection
+                            .find(find_param)
+                            .sort(sort_param)
+                            .skip(skip)
+                            .limit(limit)
+                            .toArray((err, docs) => {
+                                if (err) {
+                                    reject(err);
+                                }
+                                resolve(docs);
+                            });
+                    } else if ((sort_param, limit)) {
+                        mongoDbCollection
+                            .find(find_param)
+                            .sort(sort_param)
+                            .limit(limit)
+                            .toArray((err, docs) => {
+                                if (err) {
+                                    reject(err);
+                                }
 
-                        mongoDbCollection.find(find_param).sort(sort_param).skip(skip).limit(limit).toArray((err, docs) => {
-                            if (err) { reject(err); }
-                            resolve(docs);
-                        });
-
-                    } else if (sort_param, limit) {
-
-                        mongoDbCollection.find(find_param).sort(sort_param).limit(limit).toArray((err, docs) => {
-                            if (err) { reject(err); }
-
-                            resolve(docs);
-                        });
-
+                                resolve(docs);
+                            });
                     } else if (sort_param) {
-
-                        mongoDbCollection.find(find_param).sort(sort_param).toArray((err, docs) => {
-                            if (err) { reject(err); }
-                            resolve(docs);
-                        });
-
+                        mongoDbCollection
+                            .find(find_param)
+                            .sort(sort_param)
+                            .toArray((err, docs) => {
+                                if (err) {
+                                    reject(err);
+                                }
+                                resolve(docs);
+                            });
                     } else {
-
                         if (skip && limit) {
-
-                            mongoDbCollection.find(find_param).sort(sort_param).limit(limit).toArray((err, docs) => {
-                                if (err) { reject(err); }
-                                resolve(docs);
-                            });
+                            mongoDbCollection
+                                .find(find_param)
+                                .sort(sort_param)
+                                .limit(limit)
+                                .toArray((err, docs) => {
+                                    if (err) {
+                                        reject(err);
+                                    }
+                                    resolve(docs);
+                                });
                         } else if (limit) {
-
-                            mongoDbCollection.find(find_param).limit(limit).toArray((err, docs) => {
-                                if (err) { reject(err); }
-                                resolve(docs);
-                            });
+                            mongoDbCollection
+                                .find(find_param)
+                                .limit(limit)
+                                .toArray((err, docs) => {
+                                    if (err) {
+                                        reject(err);
+                                    }
+                                    resolve(docs);
+                                });
                         } else {
-
-                            mongoDbCollection.find(find_param).toArray((err, docs) => {
-                                if (err) { reject(err); }
-                                resolve(docs);
-                            });
+                            mongoDbCollection
+                                .find(find_param)
+                                .toArray((err, docs) => {
+                                    if (err) {
+                                        reject(err);
+                                    }
+                                    resolve(docs);
+                                });
                         }
                     }
                 });
@@ -342,16 +369,21 @@ export default class MongoDb {
             findById: (id) => {
                 return new Promise((resolve, reject) => {
                     mongoDbCollection.findOne({ _id: id }, (err, doc) => {
-                        if (err) { reject(err); }
+                        if (err) {
+                            reject(err);
+                        }
                         resolve(doc);
                     });
                 });
             },
 
-            delete: (id) => { //TODO: delete many
+            delete: (id) => {
+                //TODO: delete many
                 return new Promise((resolve, reject) => {
                     mongoDbCollection.removeOne({ _id: id }, (err, result) => {
-                        if (err) { reject(err); }
+                        if (err) {
+                            reject(err);
+                        }
                         resolve(result);
                     });
                 });
@@ -360,7 +392,9 @@ export default class MongoDb {
             findOne: (param) => {
                 return new Promise((resolve, reject) => {
                     mongoDbCollection.findOne(param, (err, doc) => {
-                        if (err) { reject(err); }
+                        if (err) {
+                            reject(err);
+                        }
                         resolve(doc);
                     });
                 });
@@ -369,67 +403,70 @@ export default class MongoDb {
             count: (param) => {
                 return new Promise((resolve, reject) => {
                     mongoDbCollection.countDocuments(param, (err, doc) => {
-                        if (err) { reject(err); }
+                        if (err) {
+                            reject(err);
+                        }
                         resolve(doc);
                     });
                 });
             },
 
             get_find_cusor: (find_param, sort_param, skip, limit) => {
-
                 return new Promise((resolve, reject) => {
-
                     if (sort_param && skip && limit) {
-
-                        let cursor = mongoDbCollection.find(find_param).sort(sort_param).skip(skip).limit(limit)
-                        resolve(cursor)
-
-                    } else if (sort_param, limit) {
-
-                        let cursor = mongoDbCollection.find(find_param).sort(sort_param).limit(limit)
-                        resolve(cursor)
-
+                        let cursor = mongoDbCollection
+                            .find(find_param)
+                            .sort(sort_param)
+                            .skip(skip)
+                            .limit(limit);
+                        resolve(cursor);
+                    } else if ((sort_param, limit)) {
+                        let cursor = mongoDbCollection
+                            .find(find_param)
+                            .sort(sort_param)
+                            .limit(limit);
+                        resolve(cursor);
                     } else if (sort_param) {
-
-                        let cursor = mongoDbCollection.find(find_param).sort(sort_param)
-                        resolve(cursor)
-
+                        let cursor = mongoDbCollection
+                            .find(find_param)
+                            .sort(sort_param);
+                        resolve(cursor);
                     } else {
-
                         if (skip && limit) {
-
-                            let cursor = mongoDbCollection.find(find_param).sort(sort_param).limit(limit)
-                            resolve(cursor)
-
+                            let cursor = mongoDbCollection
+                                .find(find_param)
+                                .sort(sort_param)
+                                .limit(limit);
+                            resolve(cursor);
                         } else if (limit) {
-
-                            let cursor = mongoDbCollection.find(find_param).limit(limit)
-                            resolve(cursor)
+                            let cursor = mongoDbCollection
+                                .find(find_param)
+                                .limit(limit);
+                            resolve(cursor);
                         } else {
-
-                            let cursor = mongoDbCollection.find(find_param)
-                            resolve(cursor)
-
+                            let cursor = mongoDbCollection.find(find_param);
+                            resolve(cursor);
                         }
                     }
                 });
             },
 
             get_next_from_cusor: (cursor) => {
-
                 // console.log("cursor: ", cursor)
 
                 return new Promise((resolve, reject) => {
                     cursor.next((err, item) => {
                         if (err) {
-                            reject(err)
+                            reject(err);
                         } else {
-                            resolve(item)
+                            resolve(item);
                         }
-                    })
+                    });
                 });
             },
-        }
+        };
         return collection;
     }
-}
+};
+
+// module.exports = {MongoDb};
