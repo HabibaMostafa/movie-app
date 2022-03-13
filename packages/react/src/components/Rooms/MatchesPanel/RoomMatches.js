19c4b9bfc633a23e9c4ae6fa54977366f20636ae
jsubang <em.subang@gmail.com>
2022-03-13 17:04:54 -0400

    room match function completed

--- packages/react/src/components/Rooms/MatchesPanel/RoomMatches.js
+++ packages/react/src/components/Rooms/MatchesPanel/RoomMatches.js
@@ -4,7 +4,6 @@ import axios from "axios";
 import MovieListElement from "../../Movie/MovieListElement";
 import ImageList from "@mui/material/ImageList";
 
-
 class RoomMatches extends React.Component {
     constructor(props) {
         if (props === {}) {
@@ -12,20 +11,51 @@ class RoomMatches extends React.Component {
         }
         super(props);
         this.props = props;
+        this.state = { matches: [], fetchedData: false };
+    }
+
+    componentDidMount() {
+        axios
+            .get(`/room-matches?roomId=${this.props.roomId}`)
+            .then((result) => {
+                if (result.status === 200) {
+                    this.setState({ matches: result.data });
+                }
+
+                this.setState({ fetchedData: true });
+            });
     }
 
     render() {
-        if (true) {
-            return (
-                <div>
-                    <p>Room Matches</p>
+        if (this.state.matches !== []) {
+            if (this.state.matches.length > 0) {
+                return (
+                    <div>
+                        {/* <p>Room Matches</p>
 
-                    <p>roomId: {this.props.roomId}</p>
-                    <p>user: Id {this.props.userId}</p>
-                </div>
-            );
+                        <p>roomId: {this.props.roomId}</p>
+                        <p>user: Id {this.props.userId}</p> */}
+
+                        <ImageList
+                            // sx={{ width: 1900, height: 450 }}
+                            cols={6}
+                            rowHeight={164}
+                        >
+                            {this.state.matches.map((value) => (
+                                <MovieListElement movieID={value} />
+                            ))}
+                        </ImageList>
+                    </div>
+                );
+            }
+
+            if (!this.state.fetchedData) {
+                return <div>loading...</div>;
+            } else {
+                return <div>No Matches =(</div>;
+            }
         } else {
-            return <div></div>;
+            return <div>No Matches =(</div>;
         }
     }
 }
