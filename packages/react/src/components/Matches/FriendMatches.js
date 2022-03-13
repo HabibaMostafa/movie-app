d6a58aaac87fdab6284c975cf5ddc2d23c79a24c
jsubang <em.subang@gmail.com>
2022-03-13 17:59:46 -0400

    added more movie columns in matches

--- packages/react/src/components/Matches/FriendMatches.js
+++ packages/react/src/components/Matches/FriendMatches.js
@@ -84,7 +84,7 @@ class FriendMatches extends React.Component {
                     // <p>{value.movieID}</p>
                     ))} */}
 
-                <ImageList sx={{ width: 900, height: 450 }} cols={4} rowHeight={164}>
+                <ImageList sx={{ width: 1800, height: 450 }} cols={7} rowHeight={164}>
                 {this.state.matches.map((value) => (
                     <MovieListElement movieID={value.movieID} />
                     // <ImageListItem key={item.img}>
